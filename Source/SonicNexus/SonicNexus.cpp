502d4deb93dc26612910e72ed2df15eeca11f83c
MegAmi <angrybirdconnor@gmail.com>
2024-12-07 13:56:47 -0600

    Fix public functions

--- Source/SonicNexus/SonicNexus.cpp
+++ Source/SonicNexus/SonicNexus.cpp
@@ -1,15 +1,10 @@
 #include "SonicNexus.hpp"
 #include "Math.hpp"
+#if RETRO_USE_MOD_LOADER
+#include "PublicFunctions.hpp"
+#endif
 
 using namespace RSDK;
-
-#if RETRO_USE_MOD_LOADER
-extern "C" {
-DLLExport bool32 LinkModLogic(EngineInfo *info, const char *id);
-}
-
-const char *modID = "SonicNexus";
-#endif
 
 GlobalVariables::Constructor c;
 GlobalVariables *globals = nullptr;
@@ -18,16 +13,28 @@ void GlobalVariables::Init(void *g)
 {
     GlobalVariables *globals = (GlobalVariables *)g;
     memset(globals, 0, sizeof(GlobalVariables));
+}
 
+void LinkGameLogic(RSDK::EngineInfo *info)
+{
     sceneInfo->debugMode = false;
-
     GameLogic::CalculateTrigAngles();
+#if RETRO_USE_MOD_LOADER
+    GameLogic::InitPublicFunctions();
+#endif
 }
 
 #if RETRO_USE_MOD_LOADER
-#include "PublicFunctions.hpp"
+bool32 LinkModLogic(RSDK::EngineInfo *info, const char *modID)
+{
+#if RETRO_REV02
+    LinkGameLogicDLL(info);
+#else
+    LinkGameLogicDLL(*info);
+#endif
 
-void InitModAPI(void) { GameLogic::InitPublicFunctions(); }
+    RSDK::Mod::modID = modID;
 
-void InitModAPI(void);
+    return true;
+}
 #endif
